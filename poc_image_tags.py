3d2ed063f465bc173b28602c2029d9b7e60a0b6c
Francisco Machado e Costa <frmachadoecosta@gmail.com>
2025-11-17 12:03:18 +0000

    renamed secrets

--- poc_image_tags.py
+++ poc_image_tags.py
@@ -1,7 +1,7 @@
 from azure.ai.vision.imageanalysis import ImageAnalysisClient
 from azure.ai.vision.imageanalysis.models import VisualFeatures
 from azure.core.credentials import AzureKeyCredential
-from secrets import key, endpoint
+from azure_secrets import key, endpoint
 
 client = ImageAnalysisClient(
     endpoint=endpoint,
