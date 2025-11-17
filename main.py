e0883a21bf46cb965258e2ff759df687ef323b56
Francisco Machado e Costa <frmachadoecosta@gmail.com>
2025-11-17 12:03:32 +0000

    hello world

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import streamlit as st
-from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
+from azure_secrets import key, endpoint
+
+
+client = ImageAnalysisClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(key)
+)
+
+
+
+def main():
+
+    st.title("Azure Computer Vision OCR")
+    st.write("This app uses Azure Computer Vision to extract text from an uploaded image.")
+
+    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+
+
+if __name__ == "__main__":
+    main()
